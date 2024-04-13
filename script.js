37871eb71be8923bb10fabd2bc3ab5780f0b7b58
Even <even.berhane2022@gmail.com>
2024-04-13 20:06:14 +0300

    js code

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
