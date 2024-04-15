4d2b39a5cd5cf9cef38e9142eec0e89d02a93c25
Even <even.berhane2022@gmail.com>
2024-04-15 19:56:27 +0300

    Declared a new HTMLString variable

--- script.js
+++ script.js
@@ -18,4 +18,6 @@ function cleanInputString(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    let HTMLString = ``;
+
   }
